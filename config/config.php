ad2579fe2cf68fb68d3a3efa0b3ed83ebf50fb96
ElFirdaous28 <elmokhtarifirdaous@gmqil.com>
2025-02-04 16:16:27 +0100

    structur 1

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'app_name' => 'My PHP Framework',
+    'debug' => true,
+    'database' => [
+        'host' => 'localhost',
+        'user' => 'root',
+        'password' => '',
+        'dbname' => 'my_database',
+    ],
+];
