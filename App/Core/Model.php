43f7c01f0a6d7d283af94f52f578691ec7290421
ElFirdaous28 <elmokhtarifirdaous@gmqil.com>
2025-02-07 09:04:09 +0100

    MMAPE-12 MMAPE-14 auth.php and AuthController

--- App/Core/Model.php
+++ App/Core/Model.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class Model extends Eloquent
 {
-    protected $guarded = ['id']; // Prevent mass assignment of `id`
-    public $timestamps = true;   // Enable timestamps (if your table has `created_at` & `updated_at`)
+    protected $guarded = ['id'];
+    public $timestamps = true;
 }
